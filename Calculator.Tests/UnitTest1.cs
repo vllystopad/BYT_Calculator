f876eb5cad2c521cef933e371664b4a04555c02d
ListopadV <vladlistopad6@gmail.com>
2025-11-11 19:01:40 +0100

    full project with tests & calculator

---
+++ Calculator.Tests/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿using Tut2_s30472;
+
+namespace Tut2_s30472_Tests
+{
+    public class CalculatorTests
+    {
+        [TestCase(5, 3, '+', ExpectedResult = 8)]
+        [TestCase(5, 3, '-', ExpectedResult = 2)]
+        [TestCase(5, 3, '*', ExpectedResult = 15)]
+        [TestCase(6, 3, '/', ExpectedResult = 2)]
+        public double TestBasicOperations(double a, double b, char op)
+        {
+            var calc = new Calculator(a, b, op);
+            return calc.Calculate();
+        }
+
+        [Test]
+        public void TestDivisionByZero()
+        {
+            var calc = new Calculator(5, 0, '/');
+            Assert.Throws<DivideByZeroException>(() => calc.Calculate());
+        }
+
+        [Test]
+        public void TestInvalidOperation()
+        {
+            var calc = new Calculator(5, 5, '%');
+            Assert.Throws<InvalidOperationException>(() => calc.Calculate());
+        }
+
+        [TestCase(-5, 3, '+', ExpectedResult = -2)]
+        [TestCase(-5, -3, '*', ExpectedResult = 15)]
+        [TestCase(0, 0, '+', ExpectedResult = 0)]
+        [TestCase(0, 5, '*', ExpectedResult = 0)]
+        public double TestEdgeCases(double a, double b, char op)
+        {
+            var calc = new Calculator(a, b, op);
+            return calc.Calculate();
+        }
+
+        [Test]
+        public void TestNaNOperands()
+        {
+            var calc1 = new Calculator(double.NaN, 5, '+');
+            Assert.Throws<ArgumentException>(() => calc1.Calculate());
+            var calc2 = new Calculator(5, double.NaN, '*');
+            Assert.Throws<ArgumentException>(() => calc2.Calculate());
+        }
+
+        [Test]
+        public void TestInfinityOperands()
+        {
+            var calc1 = new Calculator(double.PositiveInfinity, 5, '+');
+            Assert.Throws<ArgumentException>(() => calc1.Calculate());
+            var calc2 = new Calculator(5, double.NegativeInfinity, '*');
+            Assert.Throws<ArgumentException>(() => calc2.Calculate());
+        }
+
+        [Test]
+        public void TestOverflow()
+        {
+            var calc = new Calculator(1e308, 1e308, '+');
+            Assert.Throws<OverflowException>(() => calc.Calculate());
+            var calc2 = new Calculator(1e154, 1e154, '*');
+            Assert.Throws<OverflowException>(() => calc2.Calculate());
+        }
+    }
+}
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,68 @@
+﻿namespace Tut2_s30472
+{
+    public class Calculator
+    {
+        private double A { get; }
+        private double B { get; }
+        private char Operation { get; }
+
+        public Calculator(double a, double b, char operation)
+        {
+            A = a;
+            B = b;
+            Operation = operation;
+        }
+
+        public double Calculate()
+        {
+            if (double.IsNaN(A) || double.IsNaN(B))
+                throw new ArgumentException("Operands cannot be NaN.");
+            if (double.IsInfinity(A) || double.IsInfinity(B))
+                throw new ArgumentException("Operands cannot be Infinity.");
+
+            checked
+            {
+                return Operation switch
+                {
+                    '+' => A + B,
+                    '-' => A - B,
+                    '*' => A * B,
+                    '/' => B != 0 ? A / B : throw new DivideByZeroException("Cannot divide by zero."),
+                    _ => throw new InvalidOperationException($"Invalid operation: {Operation}")
+                };
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var testCases = new (double, double, char)[]
+            {
+                (10, 5, '+'),
+                (10, 5, '-'),
+                (10, 5, '*'),
+                (10, 5, '/'),
+                (10, 0, '/'),
+                (double.NaN, 5, '+'),
+                (5, double.PositiveInfinity, '*'),
+                (1e308, 1e308, '+'),
+                (5, 5, '%')
+            };
+
+            foreach (var (a, b, op) in testCases)
+            {
+                var calc = new Calculator(a, b, op);
+                try
+                {
+                    Console.WriteLine($"{a} {op} {b} = {calc.Calculate():F2}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error performing {a} {op} {b}: {ex.Message}");
+                }
+            }
+        }
+    }
+}
